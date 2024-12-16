43d89938e811cf5bb264f2637bbc7ca36b2a1aec
Onton <bashprog@gmail.com>
2024-12-16 09:56:18 +0600

    Add styles, phone img and container

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import phone from './imgs/phone.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="centered">
+      <img src={phone} width={1030}/>
+      <div className='mobileContainer'>
+        pivo pivo
+      </div>
     </div>
   );
 }
 
 export default App;
+
+// Это родительский компонент, который рендериться в index.js и с него начинается расхождение дерева, картинку дерева после заверщнеия проекта нарисую
+// Стили буду писать на препроцессоре SASS (синтаксис scssm, подробнее можно увидеть тут: https://sass-lang.com/)
+// Компоненты в основном будуть с material ui, ибо они там готовые 
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from "react-router";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')); 
 root.render(
-  <React.StrictMode>
+  <BrowserRouter> 
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
+//Это основной компонент который рендерит страницу, он стоит выше всех по дереву поэтому через него 
+//подлючается роутинг (Навигация по страницам на сайте) и store (хранилище данных с их последовательной мутацией)
